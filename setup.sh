6d5b08d7428735415ca09d15b73a09e003f58512
José Rafael Carrero León <josercl@gmail.com>
2012-08-21 15:45:36 -0430

    .

--- setup.sh
+++ setup.sh
@@ -2,29 +2,29 @@
 
 CFG_DIR=$PWD
 
-ln -s $CFG_DIR/bashrc ~/.bashrc
-ln -s $CFG_DIR/Xresources ~/.Xresources
+ln -s $CFG_DIR/bashrc ~/.bashrc -f
+ln -s $CFG_DIR/Xresources ~/.Xresources -f
 
 #configuracion global de git
-ln -s $CFG_DIR/git/gitconfig ~/.gitconfig
-ln -s $CFG_DIR/git/gitignore ~/.gitignore
+ln -s $CFG_DIR/git/gitconfig ~/.gitconfig -f
+ln -s $CFG_DIR/git/gitignore ~/.gitignore -f
 
 #Configuracion de conky
-ln -s $CFG_DIR/conky/ ~/.conky
-ln -s $CFG_DIR/conky/conkyrc ~/.conkyrc
+ln -s $CFG_DIR/conky/ ~/.conky -f
+ln -s $CFG_DIR/conky/conkyrc ~/.conkyrc -f
 
 #Configuracion de MPD y ncmpcpp (MP3)
-ln -s $CFG_DIR/mpd ~/.mpd
-ln -s $CFG_DIR/ncmpcpp ~/.ncmpcpp
+ln -s $CFG_DIR/mpd ~/.mpd -f
+ln -s $CFG_DIR/ncmpcpp ~/.ncmpcpp -f
 
 #Configuracion de vi con perfil de colores y plugins
-ln -s $CFG_DIR/vim ~/.vim
-ln -s $CFG_DIR/vimrc ~/.vimrc
+ln -s $CFG_DIR/vim ~/.vim -f
+ln -s $CFG_DIR/vimrc ~/.vimrc -f
 
 #Configuracion de openbox
-ln -s $CFG_DIR/openbox/ob-randrrc ~/.ob-randrrc
-ln -s $CFG_DIR/openbox ~/.config/openbox
-ln -s $CFG_DIR/tint2 ~/.config/tint2
+ln -s $CFG_DIR/openbox/ob-randrrc ~/.ob-randrrc -f
+ln -s $CFG_DIR/openbox ~/.config/openbox -f
+ln -s $CFG_DIR/tint2 ~/.config/tint2 -f
 
 #Tema reNIX de openbox
-ln -s $CFG_DIR/themes/reNIX/openbox-3 ~/.themes/reNIX/openbox-3
+ln -s $CFG_DIR/themes/reNIX/openbox-3 ~/.themes/reNIX/openbox-3 -f
